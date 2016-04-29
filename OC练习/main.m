ba69149b3df3b48c248346c3c21cf7fc09d71158
wubo <levbf@wubo.local>
2016-04-29 15:20:45 +0800

    Initial Commit

---
+++ OC练习/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC练习
+//
+//  Created by wubo on 16/4/29.
+//  Copyright © 2016年 wubo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
